869e633d44b52c7502996a4568c52c76bf3e074d
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-07-11 09:19:02 +0530

    changes

--- output.tf
+++ output.tf
@@ -1,10 +1,12 @@
-variable "resource_group_location" {
-  default     = "eastus"
-  description = "Location of the resource group."
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-variable "prefix" {
-  type        = string
-  default     = "win-vm-iis"
-  description = "Prefix of the resource name"
+output "public_ip_address" {
+  value = azurerm_windows_virtual_machine.main.public_ip_address
+}
+
+output "admin_password" {
+  sensitive = true
+  value     = azurerm_windows_virtual_machine.main.admin_password
 }
